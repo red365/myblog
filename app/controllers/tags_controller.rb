86f917b56254e6d5d46b1bebc0daa40cb01832ef
Rob <rob.edwards101@googlemail.com>
2017-09-01 16:35:41 +0100

    added destroy functionality to tagging

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,10 +1,27 @@
 class TagsController < ApplicationController
+  http_basic_authenticate_with name: "red365",
+                               password: "jobsite_log1n",
+                               except: [:index, :show]
+  
   def show
     @tag = Tag.find(params[:id])
   end
 
   def index
     @tags = Tag.all
+    if @tags.last
+      @tags
+    else
+      render template: "tags/none"
+    end
+  end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+
+    redirect_to tags_path
+
   end
     
 end
